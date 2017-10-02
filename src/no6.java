3007d02bd647e04107c0ce9e14b50676035103d6
SUCHIAWEI <32055784+SUCHIAWEI@users.noreply.github.com>
2017-10-02 21:57:15 +0800

    Revert "j-SUCHIAWEI"
    This reverts commit 78555d86f488b3609c036801edba2d02b591ef0d.

--- src/no6.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class no6 {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String str= scn
-                char v1=
-}}
